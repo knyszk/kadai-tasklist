f309a16aeab28b52578e3f9737344c88795891cf
EC2 Default User <ec2-user@ip-172-31-5-4.ec2.internal>
2020-11-17 08:54:50 +0000

    user and login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.order(id: :desc)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '登録完了'
+      redirect_to tasks:index
+    else
+      flash[:danger] = '登録失敗'
+      render :new
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
